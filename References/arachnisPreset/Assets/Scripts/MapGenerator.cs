b35bce6aed0e3a4fba36c04e6fd353ef3378e261
justa-whitefox <justa.redfox2002@gmail.com>
2022-06-05 17:32:34 -0300

    adding mesh and color to terrain generation references

--- References/arachnisPreset/Assets/Scripts/MapGenerator.cs
+++ References/arachnisPreset/Assets/Scripts/MapGenerator.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour {
+    public enum DrawMode {NoiseMap, ColourMap, Mesh};
+    public DrawMode drawMode;
+
     public int mapWidth;
     public int mapHeight;
     public float noiseScale;
@@ -14,14 +17,39 @@ public class MapGenerator : MonoBehaviour {
 
     public int seed;
     public Vector2 offset;
+    public float meshHeightMultiplier;
+    public AnimationCurve meshHeightCurve;
 
     public bool autoUpdate;
+    public TerrainType[] regions;
 
     public void GenerateMap() {
         float[, ] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
+        Color[] colourMap = new Color[mapWidth * mapHeight];
+        for (int y = 0; y < mapHeight; y++) {
+            for (int x = 0; x < mapWidth; x++) {
+                float currentHeight = noiseMap[x, y];
+                for (int i = 0; i < regions.Length; i++) {
+                    if(currentHeight <= regions[i].height) {
+                        colourMap[y * mapHeight + x] = regions[i].colour;
+                        break;
+                    }
+                }
+            }
+        }
+
         MapDisplay display = FindObjectOfType<MapDisplay>();
-        display.DrawNoiseMap(noiseMap);
+        if (drawMode == DrawMode.NoiseMap) {
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+        }
+        else if (drawMode == DrawMode.ColourMap) {
+            display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+        }
+        else if (drawMode == DrawMode.Mesh) {
+            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultiplier), TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+        }
+
     }
 
     void OnValidate() {
@@ -38,4 +66,11 @@ public class MapGenerator : MonoBehaviour {
             octaves = 0;
         }
     }
+}
+
+[System.Serializable]
+public struct TerrainType {
+    public string name;
+    public float height;
+    public Color colour;
 }
